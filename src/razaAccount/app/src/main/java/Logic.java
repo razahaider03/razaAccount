5cb37c89b041d73bcb5e431d1c49d742bb0f8e08
Raza Haider <razahaider03@gmail.com>
2020-08-20 13:53:24 +0530

    new formatting

--- src/razaAccount/app/src/main/java/Logic.java
+++ src/razaAccount/app/src/main/java/Logic.java
@@ -1,96 +1,90 @@
 package razaAccount;
 
-
 /**
  * This is where the logic of this App is centralized.
- * <p/>
- * The assignments are designed this way to simplify your early
- * Android interactions.  Designing the assignments this way allows
- * you to first learn key 'Java' features without having to beforehand
- * learn the complexities of Android.
+ *
+ * <p>The assignments are designed this way to simplify your early Android interactions. Designing
+ * the assignments this way allows you to first learn key 'Java' features without having to
+ * beforehand learn the complexities of Android.
  */
-public
-class Logic extends Account
-{
+public class Logic extends Account {
 
-    /**
-     * This is the method that will ultimately get called
-     */
-
-    public static
-    void main(String[] args){
-        Account acct1=new Account();
-        Account acct2=new Account();
-        /*
-          creating array of objects and filling with values
-         */
-        Account[] customer={new Account("Rose Dior" , 389574) ,
-                new Account("Lucy Lastic" , 847004) ,
-                new Account("Viola Fuss" , 590432 , 182.90)};
-        for ( Account account : customer ) {
-            System.out.println(account.toString());
-        }
-
-        Account acctX=new Account( "Bob" , 674903 , 27.50);
-        // mOut.println(acctX.toString()); # uncomm. for print acctx.
-        tripleIt(acctX);
-        acctX.displayBalance();
-
-        // makes copy of existing object
-
-        // Account acctY = new Account(mOut); initialize a new object.
-        acct2.makeCopyOf(acctX);            // i am using existing one.
-        acct2.setName("Raza Haider");
-        System.out.println("acct2="+acct2.toString());
-
-        /*acct1.name = "Bill";
-        acct1.number = 738924;
-        acct1.balance = 231.48;
-
-        acct2.name = "Sue";
-        acct2.number = 894730;
-        acct2.balance =3000;
-        Above code is working before the fields are private
-*/
-        acct1.setName("Rick Rudd");
-        acct1.setNumber(298576);
-        acct1.setBalance(150.2);
-        System.out.println(acct1.getName());
-
-        // mOut.println("Account number:- "+acct1.getNumber());
-        System.out.println(acct1.getNumber()); //added println method to outputInterface to print integer
-        // and also in MAinActivity
-
-        System.out.println(acct1.getBalance());  // also displayBalance() works like this.
-
-        // int value = acct1.getNumber();
-        // *mOut.println(value);
-        System.out.println("before");
-        acct1.setBalance(15);  // overrides the previous setBalance
-        acct1.displayBalance();
-        acct1.deposit(18.91);
-        System.out.println("After");
-        System.out.println("Deposit is ");
-        acct1.displayBalance();
-
-
-        // acct1.deposit(89.00);
-        // acct1.displayBalance();
-
-       /* acct2.displayBalance();
-        acct2.withdrawal(300);
-        acct2.displayBalance();
-*/
-        // USing our own constructor
-        Account acctA=new Account("Sue Vlaki" , 289476);
-        Account acctB=new Account( "Joseph schmoe" , 392784 , 187.13);
-
-        System.out.println(acct1.toString());
-        System.out.println(acctB.toString());
-
-        acctA.displayBalance();
-        acctB.displayBalance();
-        acctB.withdrawal(100);
-        acctB.displayBalance();
+  /** This is the method that will ultimately get called */
+  public static void main(String[] args) {
+    Account acct1 = new Account();
+    Account acct2 = new Account();
+    /*
+     creating array of objects and filling with values
+    */
+    Account[] customer = {
+      new Account("Rose Dior", 389574),
+      new Account("Lucy Lastic", 847004),
+      new Account("Viola Fuss", 590432, 182.90)
+    };
+    for (Account account : customer) {
+      System.out.println(account.toString());
     }
+
+    Account acctX = new Account("Bob", 674903, 27.50);
+    // mOut.println(acctX.toString()); # uncomm. for print acctx.
+    tripleIt(acctX);
+    acctX.displayBalance();
+
+    // makes copy of existing object
+
+    // Account acctY = new Account(mOut); initialize a new object.
+    acct2.makeCopyOf(acctX); // i am using existing one.
+    acct2.setName("Raza Haider");
+    System.out.println("acct2=" + acct2.toString());
+
+    /*acct1.name = "Bill";
+            acct1.number = 738924;
+            acct1.balance = 231.48;
+
+            acct2.name = "Sue";
+            acct2.number = 894730;
+            acct2.balance =3000;
+            Above code is working before the fields are private
+    */
+    acct1.setName("Rick Rudd");
+    acct1.setNumber(298576);
+    acct1.setBalance(150.2);
+    System.out.println(acct1.getName());
+
+    // mOut.println("Account number:- "+acct1.getNumber());
+    System.out.println(
+        acct1.getNumber()); // added println method to outputInterface to print integer
+    // and also in MAinActivity
+
+    System.out.println(acct1.getBalance()); // also displayBalance() works like this.
+
+    // int value = acct1.getNumber();
+    // *mOut.println(value);
+    System.out.println("before");
+    acct1.setBalance(15); // overrides the previous setBalance
+    acct1.displayBalance();
+    acct1.deposit(18.91);
+    System.out.println("After");
+    System.out.println("Deposit is ");
+    acct1.displayBalance();
+
+    // acct1.deposit(89.00);
+    // acct1.displayBalance();
+
+    /* acct2.displayBalance();
+            acct2.withdrawal(300);
+            acct2.displayBalance();
+    */
+    // USing our own constructor
+    Account acctA = new Account("Sue Vlaki", 289476);
+    Account acctB = new Account("Joseph schmoe", 392784, 187.13);
+
+    System.out.println(acct1.toString());
+    System.out.println(acctB.toString());
+
+    acctA.displayBalance();
+    acctB.displayBalance();
+    acctB.withdrawal(100);
+    acctB.displayBalance();
+  }
 }
